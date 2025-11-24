d6881ad4ebbb1affcf3c9c62c3e2f8b991a594d7
Fernando Pessoa <fernandopessoa1996@gmail.com>
2025-11-24 19:51:40 -0300

    fix css

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  output: 'server',
+  base: '/fluxo-conexao',
   adapter: cloudflare(),
 });
